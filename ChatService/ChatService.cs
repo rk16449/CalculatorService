f353b200d35b8be209363eb325f440b3ae88aba1
Raj <r.kadir@se16.qmul.ac.uk>
2018-08-31 19:54:18 +0100

    removed boiler plate code, renamed GetData to GetText

---
+++ ChatService/ChatService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatService
+{
+    public class ChatService : IChatService
+    {
+        public string GetText(string value)
+        {
+            return "You said" + value;
+        }
+    }
+}
