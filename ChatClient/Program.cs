6473ae534a48779e1ee94bb23adf30a88eeb8107
Raj <r.kadir@se16.qmul.ac.uk>
2018-08-31 20:44:21 +0100

    refactored code

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -8,59 +8,83 @@ namespace ChatClient
 {
     class Program
     {
+        static ServiceReference2.ChatServiceClient proxy;
+        static string name;
+
         static void Main(string[] args)
         {
-            ServiceReference2.ChatServiceClient proxy = new ServiceReference2.ChatServiceClient();
+            // Create the new proxy
+            proxy = new ServiceReference2.ChatServiceClient();
 
+            // Store persons name
+            name = AskServiceName();
+            
+            // Continuously ask for what service to do
+            while (true)
+            {
+                // Ask what the user wants to do
+                AskUserText();
+                var input = Console.ReadLine();
+
+                // Exit loop
+                if (input.Equals("0")) break;
+
+                // Else check values
+                switch (input) {
+                    case "1":
+                        AdditionService();
+                        break;
+                    case "2":
+                        SubtractService();
+                        break;
+                }
+            }
+                
+        }
+
+        static string AskServiceName()
+        {
             // Ask for name
             Console.WriteLine("Enter your name: ");
             string name = Console.ReadLine();
             var returnedMsg = proxy.GetName(name);
             Console.WriteLine(returnedMsg);
+            return name;
+        }
 
-            // Continuously ask for what service to do
-            while (true)
+        static void AskUserText()
+        {
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("This service can do many things, select a number to try");
+            Console.WriteLine("0. To Exit ");
+            Console.WriteLine("1. Add two numbers together");
+            Console.WriteLine("2. Subtract two numbers");
+            Console.WriteLine("3. Divide two numbers");
+        }
+
+        static void SubtractService()
+        {
+
+        }
+
+        static void AdditionService()
+        {
+            Console.WriteLine("You selected to add 2 numbers");
+
+            try
             {
-                Console.WriteLine("-------------------------------------------------------");
-                Console.WriteLine("This service can do many things, select a number to try");
-                Console.WriteLine("0. To Exit ");
-                Console.WriteLine("1. Add two numbers together");
-                Console.WriteLine("2. Subtract two numbers");
-                Console.WriteLine("3. Divide two numbers");
-                var input = Console.ReadLine();
-                if (input.Equals("0"))
-                {
-                    break;
-                }
-
-
-                switch (input) {
-                    case "1":
-                        Console.WriteLine("You selected to add 2 numbers");
-
-                        try
-                        {
-                            Console.WriteLine("Number 1: ");
-                            int num1 = int.Parse(Console.ReadLine());
-                            Console.WriteLine("Number 2: ");
-                            int num2 = int.Parse(Console.ReadLine());
-                            Console.WriteLine("The numbers you are sending to the service is: " + num1 + " and " + num2);
-                            var returnedValue = proxy.AddIntegerNumbers(num1, num2);
-                            Console.WriteLine("The service replies with: " + returnedValue);
-                        }
-                        catch(Exception ex)
-                        {
-                            Console.WriteLine("Invalid integer values");
-                        }
-                        
-                        
-                        break;
-                }
-
-
-                Console.ReadKey();
+                Console.WriteLine("Number 1: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number 2: ");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("The numbers you are sending to the service is: " + num1 + " and " + num2);
+                var returnedValue = proxy.AddIntegerNumbers(num1, num2);
+                Console.WriteLine("The service replies with: " + returnedValue);
             }
-                
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid integer values");
+            }
         }
     }
 }
